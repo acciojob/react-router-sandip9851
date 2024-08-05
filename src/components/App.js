01ce25eedb3f771eceaf2c90d9c7052a06a7b2e7
sandip <mr.sandippradhan@gmail.com>
2024-08-05 22:50:57 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,31 @@
 
-import React from "react";
-import './../styles/App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+
+import Home from './Home';
+import About from './About';
 
 const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+	return (
+		<Router>
+			<div>
+				<nav>
+					<ul>
+						<li>
+							<Link to="/">Home</Link>
+						</li>
+						<li>
+							<Link to="/about">About</Link>
+						</li>
+					</ul>
+				</nav>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/about" element={<About />} />
+				</Routes>
+			</div>
+		</Router>
+	);
+};
 
-export default App
+export default App;
